3acfdea9b34419759d36fc59384227c0e48db1da
Shiny Ballzach <135516150+SporeSun@users.noreply.github.com>
2023-08-06 22:40:48 -0700

    wrote readme

---
+++ README.md
@@ -0,0 +1,25 @@
+# Weather Dashboard
+
+## Description
+
+I like to travel, and with that comes needing to know what the weather is in multiple cities. I decided to write an app calling on OpenWeather to give me a five day forecast for a vacation. I built this project to learn how to more effectively utilize server-side APIs. I learned how complex API returns are to organize and how to use slice more effectively. It solved much of my concerns around my abilities.
+
+
+## Installation
+
+N/A
+
+## Usage
+
+Enter https://sporesun.github.io/Weather-Dashboard/ into URL bar. Enter in your city of choice and it will return that day's weather and the forecast for the next 4 days.
+![results](assets/images/screenshot.png)
+
+
+## Credits
+
+AskBCS
+ChatGPT gave me immediate feedback, advice, and error corrections. Cannot thank this thing enough.
+
+## License
+
+MIT Open License
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ const apiKey = '4dd7b57e5eed893af88cf65760ecfbe4'; // Replace this with your act
 
 // Function to fetch weather data from the API
 async function getWeatherData(city) {
-  const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
+  const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`;
   
   try {
     const response = await fetch(apiUrl);
@@ -34,54 +34,33 @@ function displayCurrentWeather(cityName, date, icon, temperature, humidity, wind
     $('#weatherInfo').append(weatherInfoDiv);
   }
 
+// Create and append the elements to display the 5-day forecast
 function displayForecast(forecastData) {
     // Clear the previous forecast
     $('#forecast').empty();
-
+        
     // Create and append the elements to display the 5-day forecast
-    function displayForecast(forecastData) {
-        // Clear the previous forecast
-        $('#forecast').empty();
-        
-        // Create and append the elements to display the 5-day forecast
-        forecastData.list.slice(1, 6).forEach((forecast) => {
-            const date = forecast.dt_txt.split(' ')[0];
-            const icon = 'https://openweathermap.org/img/w/' + forecast.weather[0].icon + '.png';
-            const temperature = forecast.main.temp;
-            const humidity = forecast.main.humidity;
-            const windSpeed = forecast.wind.speed;
-        
-            const forecastItem = $('<div>').addClass('col-md-3 forecast-item');
-            const dateP = $('<p>').text('Date: ' + date);
-            const weatherIcon = $('<img>').attr('src', icon);
-            const temperatureP = $('<p>').text('Temperature: ' + temperature + ' °F');
-            const humidityP = $('<p>').text('Humidity: ' + humidity + '%');
-            const windSpeedP = $('<p>').text('Wind Speed: ' + windSpeed + ' MPH');
-        
-            forecastItem.append(dateP, weatherIcon, temperatureP, humidityP, windSpeedP);
-            $('#forecast').append(forecastItem);
-        });
-        }
+    forecastData.list.slice(1, 6).forEach((forecast) => {
+        const date = forecast.dt_txt.split(' ')[0];
+        const icon = 'https://openweathermap.org/img/w/' + forecast.weather[0].icon + '.png';
+        const temperature = forecast.main.temp;
+        const humidity = forecast.main.humidity;
+        const windSpeed = forecast.wind.speed;
+    
+        const forecastItem = $('<div>').addClass('col-md-3 forecast-item');
+        const dateP = $('<p>').text('Date: ' + date);
+        const weatherIcon = $('<img>').attr('src', icon);
+        const temperatureP = $('<p>').text('Temperature: ' + temperature + ' °F');
+        const humidityP = $('<p>').text('Humidity: ' + humidity + '%');
+        const windSpeedP = $('<p>').text('Wind Speed: ' + windSpeed + ' MPH');
+    
+        forecastItem.append(dateP, weatherIcon, temperatureP, humidityP, windSpeedP);
+        $('#forecast').append(forecastItem);
+    });
+    }
 
   // Create and append the elements to display the 5-day forecast
-  forecastData.list.slice(1, 6).forEach((forecast) => {
-    const date = forecast.dt_txt.split(' ')[0];
-    const icon = 'https://openweathermap.org/img/w/' + forecast.weather[0].icon + '.png';
-    const temperature = forecast.main.temp;
-    const humidity = forecast.main.humidity;
-    const windSpeed = forecast.wind.speed;
 
-    const forecastItem = $('<div>').addClass('col-md-3 forecast-item');
-    const dateP = $('<p>').text('Date: ' + date);
-    const weatherIcon = $('<img>').attr('src', icon);
-    const temperatureP = $('<p>').text('Temperature: ' + temperature + ' °F');
-    const humidityP = $('<p>').text('Humidity: ' + humidity + '%');
-    const windSpeedP = $('<p>').text('Wind Speed: ' + windSpeed + ' MPH');
-
-    forecastItem.append(dateP, weatherIcon, temperatureP, humidityP, windSpeedP);
-    $('#forecast').append(forecastItem);
-  });
-}
 
 // Function to handle form submission
 async function handleSubmit(event) {
