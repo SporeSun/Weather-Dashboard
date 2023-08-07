ba9818ba3f34bae64c9cc40cdcf00293d58eee3e
Shiny Ballzach <135516150+SporeSun@users.noreply.github.com>
2023-08-06 21:26:00 -0700

    added function to display current weather info and clear previous weather info

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// API key for OpenWeatherMap
 const apiKey = '4dd7b57e5eed893af88cf65760ecfbe4'; // Replace this with your actual API key
 
 // Function to fetch weather data from the API
@@ -15,3 +16,20 @@ async function getWeatherData(city) {
     return null;
   }
 }
+
+// Function to display current weather inf
+function displayCurrentWeather(cityName, date, icon, temperature, humidity, windSpeed) {
+    // Clear the previous weather info
+    $('#weatherInfo').empty();
+  
+    // Create and append the elements to display the current weather info
+    const weatherInfoDiv = $('<div>').addClass('weather-info');
+    const cityHeader = $('<h2>').text(cityName + ' (' + date + ')');
+    const weatherIcon = $('<img>').attr('src', icon);
+    const temperatureP = $('<p>').text('Temperature: ' + temperature + ' °F');
+    const humidityP = $('<p>').text('Humidity: ' + humidity + '%');
+    const windSpeedP = $('<p>').text('Wind Speed: ' + windSpeed + ' MPH');
+  
+    weatherInfoDiv.append(cityHeader, weatherIcon, temperatureP, humidityP, windSpeedP);
+    $('#weatherInfo').append(weatherInfoDiv);
+  }
